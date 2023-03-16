62ff72c20b1b3790664e83b225fea224a594795b
Chris de Bruijn <xevichris@gmail.com>
2023-03-16 10:13:08 +0100

    v1

--- main.go
+++ main.go
@@ -1,30 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-type Data struct {
-	Low    int64
-	Medium int64
-	High   int64
+type Config struct {
+	Low    int `json:"Low"`
+	Medium int `json:"Medium"`
+	High   int `json:"High"`
 }
 
 func main() {
-	logFile1, err1 := os.OpenFile("alarmErrorLogs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err1 != nil {
-		os.Exit(1)
-	}
-	log.SetOutput(logFile1)
-	log.Println("Alarm error program initiated.")
+	var count int
+	flag.IntVar(&count, "count", 0, "number of times to sound the alarm")
+	flag.Parse()
 
-	jsonContent, err2 := os.ReadFile("./alarmConfig.json")
-	if err2 != nil {
-		log.Fatal(err2)
-		log.SetOutput(logFile1)
-		log.Println("Could not open alarmConfig.json")
+	// load config from file
+	configFile, err := os.Open("alarmConfig.json")
+	if err != nil {
+		log.Fatalf("error opening config file: %v", err)
 	}
-	fmt.Println(jsonContent)
+	defer configFile.Close()
+
+	var config Config
+	err = json.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		log.Fatalf("error decoding config file: %v", err)
+	}
+
+	// choose alarm level based on count
+	var level string
+	if count >= config.High {
+		level = "High"
+	} else if count >= config.Medium {
+		level = "Medium"
+	} else {
+		level = "Low"
+	}
+
+	// log alarm level and count
+	log.Printf("Alarm level: %s, Count: %d", level, count)
+
+	// sound the alarm
+	for i := 0; i < count; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("ALARM!", i)
+	}
 }
